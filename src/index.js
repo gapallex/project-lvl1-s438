c354558930af6efbeba06a5779069e7aa020ac0b
gapallex <gapallex@gmail.com>
2019-02-04 21:58:09 +0300

    2step, add eslint

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import readlineSync from 'readline-sync';
 export const welcome = () => {
   console.log('Welcome to the Brain Games!');
   console.log('');
-  let userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}`);
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
