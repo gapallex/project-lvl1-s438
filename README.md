2f4aff3aa131ba44744b38c46f2546bb2af03a19
gapallex <gapallex@gmail.com>
2019-02-06 14:00:17 +0300

    3step

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-[![Maintainability](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability)](https://codeclimate.com/github/codeclimate/codeclimate/maintainability)
-[![Test Coverage](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/test_coverage)](https://codeclimate.com/github/codeclimate/codeclimate/test_coverage)
+[![Maintainability](https://api.codeclimate.com/v1/badges/f41f7e9a676743fe68cb/maintainability)](https://codeclimate.com/github/gapallex/project-lvl1-s438/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/f41f7e9a676743fe68cb/test_coverage)](https://codeclimate.com/github/gapallex/project-lvl1-s438/test_coverage)
 [![Build Status](https://travis-ci.com/gapallex/project-lvl1-s438.svg?branch=master)](https://travis-ci.com/gapallex/project-lvl1-s438)
 
 # The BrainGames
--- src/index.js
+++ src/index.js
@@ -1,10 +1,56 @@
 import readlineSync from 'readline-sync';
 
-const welcome = () => {
-  console.log('Welcome to the Brain Games!');
-  console.log('');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+const userName = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return `${name}`;
 };
 
-export default welcome;
+const greeting = (rules = '') => {
+  console.log('Welcome to the Brain Games!');
+  if (rules !== '') {
+    console.log(`${rules}`);
+  }
+};
+
+
+const isEvenGame = (name, round) => {
+  const question = () => {
+    const randomIntNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+    const number = randomIntNumber(1, 1000);
+    const rightAnswer = (number % 2 === 0 ? 'yes' : 'no');
+    console.log(`Question: ${number}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === rightAnswer) {
+      console.log('Correct!');
+      return true;
+    }
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+    return false;
+  };
+  for (let count = 0; count < round; count += 1) {
+    if (question(name) === false) {
+      console.log(`Let's try again, ${name}!`);
+      break;
+    } else if (count === round - 1) {
+      console.log(`Congratulations, ${name}!`);
+      break;
+    }
+  }
+};
+
+export const welcome = () => {
+  greeting();
+  console.log('');
+  userName();
+};
+
+
+export const evenGame = () => {
+  const rules = 'Answer "yes" if number even otherwise answer "no".';
+  greeting(rules);
+  console.log('');
+  const name = userName();
+  console.log('');
+  isEvenGame(name, 3);
+};
